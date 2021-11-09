63293e74625981778ae5b13e6d72ed9ddc7118cb
Waihon Yew <yewwaihon@gmail.com>
2021-11-10 00:05:35 +0800

    Display multiple authors using #each

--- app/routes/author.js
+++ app/routes/author.js
@@ -2,9 +2,15 @@ import Route from '@ember/routing/route';
 
 export default class AuthorRoute extends Route {
   model() {
-    return {
-      first: 'J.K.',
-      last: 'Rowling',
-    }
+    return [
+      {
+        first: 'J.K.',
+        last: 'Rowling',
+      },
+      {
+        first: 'Tom',
+        last: 'Clancy',
+      }
+    ];
   }
 }
