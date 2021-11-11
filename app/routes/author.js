91780db49bc7d9035451cc40ff795e4c75eb6774
Waihon Yew <yewwaihon@gmail.com>
2021-11-11 21:42:43 +0800

    Lint and fix linting errors

--- app/routes/author.js
+++ app/routes/author.js
@@ -2,7 +2,8 @@ import Route from '@ember/routing/route';
 
 export default class AuthorRoute extends Route {
   model() {
-    return fetch('http://localhost:3000/authors')
-      .then(response => response.json());
+    return fetch('http://localhost:3000/authors').then((response) =>
+      response.json()
+    );
   }
 }
