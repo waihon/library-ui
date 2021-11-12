395e44d709ccc1f0ffbbaf050a01dfe65bd34f8a
Waihon Yew <yewwaihon@gmail.com>
2021-11-12 16:25:14 +0800

    Customize the detail route with a dynamic segment

--- app/router.js
+++ app/router.js
@@ -8,6 +8,6 @@ export default class Router extends EmberRouter {
 
 Router.map(function () {
   this.route('author', { path: '/authors' }, function () {
-    this.route('detail');
+    this.route('detail', { path: '/:id' });
   });
 });
