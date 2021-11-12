ee082368f732da3dc74ac92bd0d8c91a6fb26bf6
Waihon Yew <yewwaihon@gmail.com>
2021-11-12 15:18:20 +0800

    Replace the fetch call with Ember Data's findAll

--- app/routes/author.js
+++ app/routes/author.js
@@ -2,8 +2,6 @@ import Route from '@ember/routing/route';
 
 export default class AuthorRoute extends Route {
   model() {
-    return fetch('http://localhost:3000/authors').then((response) =>
-      response.json()
-    );
+    return this.store.findAll('author');
   }
 }
