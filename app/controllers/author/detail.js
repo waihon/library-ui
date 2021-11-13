22c56ed9e64444fd5909f46d23e18e568ac6017f
Waihon Yew <yewwaihon@gmail.com>
2021-11-14 00:05:10 +0800

    First method for handling button action: onclick

--- app/controllers/author/detail.js
+++ app/controllers/author/detail.js
@@ -1,3 +1,10 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 
-export default class AuthorDetailController extends Controller {}
+export default class AuthorDetailController extends Controller {
+  @action
+  deleteAuthor() {
+    console.log(this);
+    window.alert('Delete Author');
+  }
+}
