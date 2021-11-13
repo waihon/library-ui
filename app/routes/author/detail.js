8cc9b84d8bf36a657282ac8fefa0678774932665
Waihon Yew <yewwaihon@gmail.com>
2021-11-13 23:03:27 +0800

    Use Emeber Data to find a single author from our app using its ID

--- app/routes/author/detail.js
+++ app/routes/author/detail.js
@@ -3,10 +3,6 @@ import Route from '@ember/routing/route';
 export default class AuthorDetailRoute extends Route {
   // Destructure out the id value of the params object
   model({ id }) {
-    return {
-      first: 'J.K.',
-      last: 'Rowling',
-      id: `author-${id}`,
-    }
+    return this.store.findRecord('author', id);
   }
 }
