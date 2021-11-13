75c355816de7d24d45f193bbff4b7efdccec93e8
Waihon Yew <yewwaihon@gmail.com>
2021-11-13 22:44:05 +0800

    Make use of dynamic data inside route templates using Handlebars

--- app/routes/author/detail.js
+++ app/routes/author/detail.js
@@ -1,3 +1,12 @@
 import Route from '@ember/routing/route';
 
-export default class AuthorDetailRoute extends Route {}
+export default class AuthorDetailRoute extends Route {
+  // Destructure out the id value of the params object
+  model({ id }) {
+    return {
+      first: 'J.K.',
+      last: 'Rowling',
+      id: `author-${id}`,
+    }
+  }
+}
