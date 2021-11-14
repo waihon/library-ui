80740409a51aac9473d1c96f5ef4b57abd3681db
Waihon Yew <yewwaihon@gmail.com>
2021-11-14 23:46:59 +0800

    Save changes made to an author and transtion back to author route

--- app/controllers/author/edit.js
+++ app/controllers/author/edit.js
@@ -1,3 +1,16 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
-export default class AuthorEditController extends Controller {}
+export default class AuthorEditController extends Controller {
+  @service router;
+
+  @action
+  saveAuthor(formValues) {
+    this.model.setProperties(formValues);
+
+    this.model.save().then(() => {
+      this.router.transitionTo('author');
+    })
+  }
+}
