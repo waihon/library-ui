7a193bba78106c69574739f4ebde4a7774c4ddf0
Waihon Yew <yewwaihon@gmail.com>
2021-11-14 16:39:57 +0800

    Create an author locally

--- app/controllers/author/create.js
+++ app/controllers/author/create.js
@@ -1,3 +1,19 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
-export default class AuthorCreateController extends Controller {}
+export default class AuthorCreateController extends Controller {
+  @service router;
+
+  @action
+  saveAuthor(event) {
+    // Prevent the default behaviour of submitting a form:
+    // sending a network request and clering the fields.
+    event.preventDefault();
+
+    // Create an author locally
+    this.store.createRecord('author', this.model);
+
+    this.router.transitionTo('author');
+  }
+}
