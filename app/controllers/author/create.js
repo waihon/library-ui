59d1bdf3605b02791d866f3a0bde86440cb795c2
Waihon Yew <yewwaihon@gmail.com>
2021-11-14 15:34:29 +0800

    Work with inputs and input changes and handle data bindings

--- app/controllers/author/create.js
+++ app/controllers/author/create.js
@@ -1,3 +1,15 @@
 import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
-export default class AuthorCreateController extends Controller {}
+export default class AuthorCreateController extends Controller {
+  @tracked first = '';
+  @tracked last = '';
+
+  @action
+  changeFirst(value) {
+    // Use value instead of event as the function's argument to make
+    // the controller pure JavaScript.
+    this.first = value;
+  }
+}
