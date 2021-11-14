7d7aecce4340a1c50e6486b5b7f087f9391c6524
Waihon Yew <yewwaihon@gmail.com>
2021-11-14 11:54:56 +0800

    Delete an author and immediately transition back to author.index route

--- app/controllers/author/detail.js
+++ app/controllers/author/detail.js
@@ -1,10 +1,19 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class AuthorDetailController extends Controller {
+  @service router;
+
   @action
   deleteAuthor(author) {
-    console.log(author);
-    window.alert('Delete Author');
+    // Schedule a model to be deleted, i.e. it doesn't make a network request
+    // to the server.
+    author.deleteRecord();
+    // Send a network request to the server to save the changes, i.e. delete
+    // an author in this case.
+    author.save();
+
+    this.router.transitionTo('author.index');
   }
 }
