3f41bd4f7e1feff2f8291a271b9d4de849d17ac4
Waihon Yew <yewwaihon@gmail.com>
2021-11-14 16:20:45 +0800

    Replace controller properties with model properties

--- app/controllers/author/create.js
+++ app/controllers/author/create.js
@@ -1,22 +1,3 @@
 import Controller from '@ember/controller';
-import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
 
-export default class AuthorCreateController extends Controller {
-  @tracked first = '';
-  @tracked last = '';
-
-  reset() {
-    this.setProperties({
-      first: '',
-      last: '',
-    });
-  }
-
-  @action
-  changeFirst(value) {
-    // Use value instead of event as the function's argument to make
-    // the controller pure JavaScript.
-    this.first = value;
-  }
-}
+export default class AuthorCreateController extends Controller {}
--- app/routes/author/create.js
+++ app/routes/author/create.js
@@ -1,9 +1,11 @@
 import Route from '@ember/routing/route';
 
 export default class AuthorCreateRoute extends Route {
-  resetController(controller) {
-    this._super(...arguments);
-
-    controller.reset();
+  // The model function is run everytime we enter the route
+  model() {
+    return {
+      first: '',
+      last: '',
+    }
   }
 }
