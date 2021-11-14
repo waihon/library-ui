2ee5b8445e376c7e5218354e023d8a9da9a4968d
Waihon Yew <yewwaihon@gmail.com>
2021-11-14 12:15:06 +0800

    Generate a route called author.create

--- app/router.js
+++ app/router.js
@@ -9,5 +9,6 @@ export default class Router extends EmberRouter {
 Router.map(function () {
   this.route('author', { path: '/authors' }, function () {
     this.route('detail', { path: '/:id' });
+    this.route('create');
   });
 });
