3dc63c5700a1a9653cda80d3c89e2667eb75b902
Waihon Yew <yewwaihon@gmail.com>
2021-11-14 23:44:49 +0800

    Implement author-form component's template and class using DDAU

--- app/components/author-form.js
+++ app/components/author-form.js
@@ -1,3 +1,22 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
-export default class AuthorFormComponent extends Component {}
+export default class AuthorFormComponent extends Component {
+  constructor() {
+    super(...arguments);
+
+    this.first = this.args.author.first;
+    this.last = this.args.author.last;
+  }
+
+  @action
+  submitChanges(event) {
+    // Prevent the form from reloading the page.
+    event.preventDefault();
+
+    this.args.onSubmit({
+      first: this.first,
+      last: this.last,
+    });
+  }
+}
