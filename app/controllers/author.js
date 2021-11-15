b7af58b0776387e15cfefe4764d3274d4933051f
Waihon Yew <yewwaihon@gmail.com>
2021-11-15 17:03:53 +0800

    Resolve search= on the URL bar when search input is blank

--- app/controllers/author.js
+++ app/controllers/author.js
@@ -1,3 +1,6 @@
 import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
 
-export default class AuthorController extends Controller {}
+export default class AuthorController extends Controller {
+  @tracked search = '';
+}
