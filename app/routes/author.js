3617d82c39126658c1b527178e5db16182010dae
Waihon Yew <yewwaihon@gmail.com>
2021-11-15 16:12:01 +0800

    Query for authors with name containing 'J'

--- app/routes/author.js
+++ app/routes/author.js
@@ -2,6 +2,13 @@ import Route from '@ember/routing/route';
 
 export default class AuthorRoute extends Route {
   model() {
+    // /authors?filter[query]=J
+    return this.store.query('author', {
+      filter: {
+        query: 'J',
+      }
+    });
+
     return this.store.findAll('author');
   }
 }
