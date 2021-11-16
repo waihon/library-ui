5be3cc148c8e3e6800119089371de93e063d1219
Waihon Yew <yewwaihon@gmail.com>
2021-11-16 23:58:05 +0800

    Set up Mirage shorthands for replacing custom route handlers

--- mirage/config.js
+++ mirage/config.js
@@ -1,20 +1,14 @@
-export default function() {
-
-  // These comments are here to help you get started. Feel free to delete them.
-
+export default function () {
   /*
     Config (with defaults).
-
     Note: these only affect routes defined *after* them!
   */
-
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
   // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
 
   /*
     Shorthand cheatsheet:
-
     this.get('/posts');
     this.post('/posts');
     this.get('/posts/:id');
@@ -23,4 +17,8 @@ export default function() {
 
     https://www.ember-cli-mirage.com/docs/route-handlers/shorthands
   */
+  this.get('/authors');
+  this.get('/authors/:id');
+  this.post('/authors');
+  this.patch('/authors/:id');
 }
