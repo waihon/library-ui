fc9da57fc4263a3067406436e0c951c5b8a1ea45
Waihon Yew <yewwaihon@gmail.com>
2021-11-22 22:29:54 +0800

    Create a form to add a new author from the book create form

--- app/components/new-author-modal.js
+++ app/components/new-author-modal.js
@@ -1,4 +1,32 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class NewAuthorModalComponent extends Component {
+  @service store;
+  @tracked showModal = false;
+  @tracked author;
+
+  constructor() {
+    super(...arguments);
+
+    this.author = {
+      first: '',
+      last: '',
+    };
+  }
+
+  @action
+  save(event) {
+    event.preventDefault();
+
+    let author = this.store.createRecord('author', this.author);
+
+    author.save().then(() => {
+      this.showModal = false;
+
+      this.args.onSave(author);
+    });
+  }
 }
