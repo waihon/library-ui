86844f2ef8adaf1e7a00621aa69d2acabadc3174
Waihon Yew <yewwaihon@gmail.com>
2021-11-22 17:28:29 +0800

    Delete a selected book

--- app/controllers/book/detail.js
+++ app/controllers/book/detail.js
@@ -1,3 +1,14 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
-export default class BookDetailController extends Controller {}
+export default class BookDetailController extends Controller {
+  @service router;
+
+  @action
+  destroy() {
+    this.model.destroyRecord().then(() => {
+      this.router.transitionTo('book');
+    });
+  }
+}
