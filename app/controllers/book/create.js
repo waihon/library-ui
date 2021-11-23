a881d711d1b41ab1d2c7d66b6b84926aa47c1c64
Waihon Yew <yewwaihon@gmail.com>
2021-11-24 00:15:39 +0800

    Refactor book creation to use a book-form component

--- app/controllers/book/create.js
+++ app/controllers/book/create.js
@@ -1,33 +1,15 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 
 export default class BookCreateController extends Controller {
   @service router;
-  // Using @selected={{@model.author}} in the tempalte and updating
-  // this.model.author with author in the selectAuthor function doesn't
-  // seem to populate the dropdown with the selected author.
-  // So, we define  tracked property instead.
-  @tracked selectedAuthor;
 
   @action
-  selectAuthor(author) {
-    this.selectedAuthor = author;
-  }
-
-  @action
-  searchAuthor(query) {
-    return this.store.query('author', { filter: { query } });
-  }
-
-  @action
-  saveBook(event) {
-    // Stop the browser from reloading due to form submission
-    event.preventDefault();
-
-    this.model.author = this.selectedAuthor;
-    const book = this.store.createRecord('book', this.model);
+  saveBook(attrs) {
+    // We cannot call setProperties on this.model like in book/edit.js
+    // because this.model returns a POJO instead of an Ember object.
+    const book = this.store.createRecord('book', attrs);
 
     book.save().then(() => {
       this.router.transitionTo('book');
