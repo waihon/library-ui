d98b9a684173e392363996e32d7c01ac215aa8ca
Waihon Yew <yewwaihon@gmail.com>
2021-11-24 22:18:29 +0800

    Build a form to create books directly from the author detail page

--- app/controllers/author/detail/new-book.js
+++ app/controllers/author/detail/new-book.js
@@ -1,4 +1,17 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class AuthorDetailNewBookController extends Controller {
+  @service router;
+
+  @action saveBook(event) {
+    event.preventDefault();
+
+    let book = this.store.createRecord('book', this.model.book);
+
+    book.save().then(() => {
+      this.router.transitionTo('author.detail', this.model.author.id);
+    });
+  }
 }
--- app/routes/author/detail/new-book.js
+++ app/routes/author/detail/new-book.js
@@ -1,4 +1,17 @@
 import Route from '@ember/routing/route';
 
 export default class AuthorDetailNewBookRoute extends Route {
+  model() {
+    const author = this.modelFor('author.detail');
+
+    return {
+      author,
+      book: {
+        title: '',
+        isbn: '',
+        publishDate: '',
+        author,
+      }
+    }
+  }
 }
