296c3276418d62226f494cd8682297e32727dc8e
Waihon Yew <yewwaihon@gmail.com>
2021-11-29 19:30:07 +0800

    Implement the book/detail/index route and template to show a list of reviews

--- app/routes/book/detail/index.js
+++ app/routes/book/detail/index.js
@@ -1,3 +1,12 @@
 import Route from '@ember/routing/route';
 
-export default class BookDetailIndexRoute extends Route {}
+export default class BookDetailIndexRoute extends Route {
+  model() {
+    // Below is not required technically because Ember would inherit
+    // the model from the parent route if no new dynamic segments
+    // are added to a route's path.
+    // We want to be explicit here so that the intention is clear
+    // year down the road.
+    return this.modelFor('book.detail');
+  }
+}
