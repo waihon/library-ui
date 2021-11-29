948d738b507f9c353ded682297d61517ebd2af00
Waihon Yew <yewwaihon@gmail.com>
2021-11-29 19:43:31 +0800

    Implement the template and logic for review-form component

--- app/components/review-form.js
+++ app/components/review-form.js
@@ -1,3 +1,30 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
-export default class ReviewFormComponent extends Component {}
+export default class ReviewFormComponent extends Component {
+  // Component arguments:
+  // * review: A new review object
+  // * book: An existing book object for the new review
+  // * onSubmit: A function to be called when the form is submitted
+
+  constructor() {
+    super(...arguments);
+
+    const { user, body, book } = this.args.review;
+
+    this.user = user;
+    this.body = body;
+    this.book = book;
+  }
+
+  @action
+  submitChanges(event) {
+    event.preventDefault();
+
+    this.args.onSubmit({
+      user: this.user,
+      body: this.body,
+      book: this.book,
+    });
+  }
+}
