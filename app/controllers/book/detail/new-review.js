b6bcdf7cb4bf1ff0e3335f5302678974520fd70b
Waihon Yew <yewwaihon@gmail.com>
2021-11-29 19:35:58 +0800

    Implement the book/detail/new-review route, template, and controller

--- app/controllers/book/detail/new-review.js
+++ app/controllers/book/detail/new-review.js
@@ -1,4 +1,16 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class BookDetailNewReviewController extends Controller {
+  @service router;
+
+  @action
+  saveReview(attrs) {
+    let review = this.store.createRecord('review', attrs);
+
+    review.save().then(() => {
+      this.router.transitionTo('book.detail', this.model.book.id);
+    });
+  }
 }
--- app/routes/book/detail/new-review.js
+++ app/routes/book/detail/new-review.js
@@ -1,4 +1,16 @@
 import Route from '@ember/routing/route';
 
 export default class BookDetailNewReviewRoute extends Route {
+  model() {
+    let book = this.modelFor('book.detail');
+
+    return {
+      book,
+      review: {
+        user: '',
+        body: '',
+        book,
+      },
+    };
+  }
 }
