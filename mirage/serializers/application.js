b25ce37407c220cec37479509447b7b02aa08166
Waihon Yew <yewwaihon@gmail.com>
2021-11-30 23:55:27 +0800

    Make the Mirage serializer returns a response similar to the real API server

--- mirage/serializers/application.js
+++ mirage/serializers/application.js
@@ -1,3 +1,25 @@
 import { JSONAPISerializer } from 'ember-cli-mirage';
 
-export default JSONAPISerializer.extend({});
+export default JSONAPISerializer.extend({
+  // Make the Mirage serializer returns a response very similar to what our
+  // real bankend does.
+  links(resource) {
+    let { id, modelName } = resource;
+    if (modelName === 'author') {
+      return {
+        books: {
+          related: `/authors/${id}/books`,
+          self: `/authors/${id}/relationships/books`,
+        },
+      };
+    }
+    if (modelName === 'book') {
+      return {
+        author: {
+          related: `/books/${id}/author`,
+          self: `/books/${id}/relationships/author`,
+        },
+      };
+    }
+  },
+});
