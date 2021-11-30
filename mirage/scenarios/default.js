6f0f9c9003ed5096ed503bfcfacf7cad98f2655f
Waihon Yew <yewwaihon@gmail.com>
2021-11-30 23:54:41 +0800

    Create authors and books when Mirage server starts up

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -1,7 +1,17 @@
-export default function (/* server */) {
+import data from '../data';
+
+export default function (server) {
   /*
     Seed your development database using your factories.
     This data will not be loaded in your tests.
   */
   // server.createList('post', 10);
+
+  data.authors.map((author) => {
+    server.create('author', author);
+  });
+
+  data.books.map((book) => {
+    server.create('book', book);
+  });
 }
