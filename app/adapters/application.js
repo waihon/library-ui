0f6c18c4fef57079a59ef24b5dd91497c1c4af57
Waihon Yew <yewwaihon@gmail.com>
2021-12-01 23:19:58 +0800

    Let Ember decides whether to use Mirage or proxy server

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,15 @@ import JSONAPIAdapter from '@ember-data/adapter/json-api';
 import ENV from 'library-ui/config/environment';
 
 export default class ApplicationAdapter extends JSONAPIAdapter {
-  // apiHost is defined in config/environment.js for both development
-  // and production but not for test which uses Mirage as a mock server.
+  // `apiHost` is defined in config/environment.js for production environment
+  // but not for development and test environments.
+  // For development environment:
+  // * Without specifying a proxy when serving the Ember app, Mirage will be
+  //   used as a mock server.
+  // * By specify a proxy when serving the Ember app, the proxy will be used
+  //   as a server instead.
+  // For test environment:
+  // * Since Ember CLI Mirage is enabled in environment.js, Mirage will always
+  //   be used as a mock server.
   host = ENV.apiHost ?? '';
 }
