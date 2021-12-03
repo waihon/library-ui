05463596d5b1c99a6e01f45fc6142726026fd2a1
Waihon Yew <yewwaihon@gmail.com>
2021-12-03 23:58:27 +0800

    Enhance author.detail route's model to include books

--- app/routes/author/detail.js
+++ app/routes/author/detail.js
@@ -3,6 +3,8 @@ import Route from '@ember/routing/route';
 export default class AuthorDetailRoute extends Route {
   // Destructure out the id value of the params object
   model({ id }) {
-    return this.store.findRecord('author', id);
+    return this.store.findRecord('author', id, {
+      include: 'books',
+    });
   }
 }
