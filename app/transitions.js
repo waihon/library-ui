eaba31c7b49691cea6c160da22ae04ca7e586cd4
Waihon Yew <yewwaihon@gmail.com>
2021-12-04 21:35:24 +0800

    Add explode transition from and back to author.index

--- app/transitions.js
+++ app/transitions.js
@@ -1,5 +1,33 @@
 const duration = 600;
 
+// Wait for the background to finish fading in before sliding the card in
+// from the right to the left.
+const slidingCardIn = [
+  {
+    pickNew: '.slide-out-bg',
+    use: ['fade', { duration }],
+  },
+  {
+    pickNew: '.slide-out-card',
+    use: ['wait', duration, { then: 'toLeft' }, { duration }],
+  },
+];
+
+// Wait for the card to finish sliding out from the left to the right
+// before fading out the background.
+// The sequence of classes appears to be significant, i.e. .slide-out-bg
+// must appear before .slide-out-card.
+const slidingCardOut = [
+  {
+    pickOld: '.slide-out-bg',
+    use: ['wait', duration, { then: 'fade' }, { duration }],
+  },
+  {
+    pickOld: '.slide-out-card',
+    use: ['toRight', { duration }],
+  },
+];
+
 export default function () {
   // Add your transitions here, like:
   //   this.transition(
@@ -26,5 +54,9 @@ export default function () {
     this.reverse('fade', { duration })
   );
 
-  this.transition(this.fromRoute('author.index'), this.use('fade'));
+  this.transition(
+    this.fromRoute('author.index'),
+    this.use('explode', ...slidingCardIn),
+    this.reverse('explode', ...slidingCardOut)
+  );
 }
