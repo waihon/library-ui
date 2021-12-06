092e3252ea867473784d9250e6c8652241d5ad14
Waihon Yew <yewwaihon@gmail.com>
2021-12-06 22:49:33 +0800

    Implement component template and class for register-form

--- app/components/register-form.js
+++ app/components/register-form.js
@@ -1,3 +1,27 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
-export default class RegisterFormComponent extends Component {}
+export default class RegisterFormComponent extends Component {
+  constructor() {
+    super(...arguments);
+
+    const { email, username, password, passwordConfirmation } = this.args.user;
+
+    this.email = email;
+    this.username = username;
+    this.password = password;
+    this.passwordConfirmation = passwordConfirmation;
+  }
+
+  @action
+  saveUser(event) {
+    event.preventDefault();
+
+    this.args.onSubmit({
+      email: this.email,
+      username: this.username,
+      password: this.password,
+      passwordConfirmation: this.passwordConfirmation,
+    });
+  }
+}
