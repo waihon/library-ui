c04ea311aafbeab267324f609654c6b8aa24625c
Waihon Yew <yewwaihon@gmail.com>
2021-12-07 23:01:49 +0800

    Implement the template and class for login-form component

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,3 +1,23 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
-export default class LoginFormComponent extends Component {}
+export default class LoginFormComponent extends Component {
+  constructor() {
+    super(...arguments);
+
+    const { email, password } = this.args.user;
+
+    this.email = email;
+    this.password = password;
+  }
+
+  @action
+  login(event) {
+    event.preventDefault();
+
+    this.args.onSubmit({
+      email: this.email,
+      password: this.password,
+    });
+  }
+}
