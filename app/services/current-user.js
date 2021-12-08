3849aa72b17e9168f91a55e0732396fd5b85acbd
Waihon Yew <yewwaihon@gmail.com>
2021-12-08 23:47:33 +0800

    Create a service to retrieve current user

--- app/services/current-user.js
+++ app/services/current-user.js
@@ -1,3 +1,20 @@
-import Service from '@ember/service';
+import Service, { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
-export default class CurrentUserService extends Service {}
+export default class CurrentUserService extends Service {
+  @service store;
+  @tracked user;
+
+  load() {
+    this.store.queryRecord('user', { me: true }).then((user) => {
+      // queryRecord() expects a single result object instead of a list of
+      // objects as expected by query().
+      // Without any cutom adapter, this queryRecord call will try to
+      // invoke '/users?me=true' by default.
+      // The URL to get the current user is actually '/users/me'.
+      // So, we need to define a custom adapter for the user model in order
+      // for Ember Data to make the right request for us.
+      this.user = user;
+    });
+  }
+}
