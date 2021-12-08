8bb30c5c6190c5f1d007ffe8db6e38ca64a6249b
Waihon Yew <yewwaihon@gmail.com>
2021-12-08 23:50:24 +0800

    Crate a user adapter to override /users?me=true with /users/me

--- app/adapters/user.js
+++ app/adapters/user.js
@@ -1,3 +1,15 @@
 import ApplicationAdapter from './application';
 
-export default class UserAdapter extends ApplicationAdapter {}
+export default class UserAdapter extends ApplicationAdapter {
+  urlForQueryRecord(query) {
+    if (query.me) {
+      // '/users?me=true' exists so let's delete it...
+      delete query.me;
+
+      // ... and replace it with '/users/me'.
+      return `${super.urlForQueryRecord(...arguments)}/me`;
+    }
+
+    return super.urlForQueryRecord(...arguments);
+  }
+}
