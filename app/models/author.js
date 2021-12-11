ee4ceecae3f2cd0ebeb081adf44b326dd60187ff
Waihon Yew <yewwaihon@gmail.com>
2021-12-11 11:32:52 +0800

    Add username to author model and template

--- app/models/author.js
+++ app/models/author.js
@@ -3,6 +3,7 @@ import Model, { attr, hasMany } from '@ember-data/model';
 export default class AuthorModel extends Model {
   @attr('string') first;
   @attr('string') last;
+  @attr('string') username;
 
   @hasMany('book') books;
 
