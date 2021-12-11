f4055ba26d0106e96c2fdc31cb73411c90c74624
Waihon Yew <yewwaihon@gmail.com>
2021-12-11 15:21:11 +0800

    Redirect register route to index route if a user has already logged in

--- app/routes/register.js
+++ app/routes/register.js
@@ -1,6 +1,11 @@
 import Route from '@ember/routing/route';
+import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
-export default class RegisterRoute extends Route {
+// UnauthenticatedRouteMixin is going to check if a user is logged in and if
+// they are it's going to redirect them to the index route.
+export default class RegisterRoute extends Route.extend(
+  UnauthenticatedRouteMixin
+) {
   model() {
     return {
       email: '',
