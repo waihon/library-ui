9295218d555dd2ec5e950d5472c63c213cc45c6e
Waihon Yew <yewwaihon@gmail.com>
2021-12-11 14:11:43 +0800

    Replace session event listeners with corresponding methods from the ESA mixin

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,7 +1,11 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+// eslint-disable-next-line ember/no-mixins
+import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-export default class ApplicationRoute extends Route {
+export default class ApplicationRoute extends Route.extend(
+  ApplicationRouteMixin
+) {
   @service session;
   @service('current-user') currentUser;
 
@@ -14,23 +18,24 @@ export default class ApplicationRoute extends Route {
     this.loadUser();
   }
 
-  constructor() {
-    super(...arguments);
+  // Replaced this.session.on('authenticationSucceeded', () => {});
+  sessionAuthenticated() {
+    super.sessionAuthenticated(...arguments);
 
-    this.session.on('invalidationSucceeded', () => {
-      // There are still a lot of data that could be loaded into the memory
-      // in our application. This means someone could get hold of information
-      // that they shouldn't have access to even though the user has clicked
-      // on Logout. Instead of using Ember's route transition, it's safer to
-      // use the browser's location API to change the URL and load the entire
-      // page. That way, our application reloads and no data is kept from
-      // one session to another.
-      window.location.replace('/login');
-    });
+    this.loadUser();
+  }
 
-    this.session.on('authenticationSucceeded', () => {
-      this.loadUser();
-    });
+  // Replaced this.session.on('invalidationSucceeded', () => {});
+  sessionInvalidated() {
+    // There are still a lot of data that could be loaded into the memory
+    // in our application. This means someone could get hold of information
+    // that they shouldn't have access to even though the user has clicked
+    // on Logout. Instead of using Ember's route transition or the default
+    // logic provided by the ESA mixin (by calling super.sessionInvalidated(
+    // ...arguments)), it's safer to use the browser's location API to change
+    // the URL and load the entire page. That way, our application reloads
+    // and no data is kept from one session to another.
+    window.location.replace('/login');
   }
 
   loadUser() {
