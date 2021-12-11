fe5857b6afaeb0bc7e21d9f81c3fb0f7a769238e
Waihon Yew <yewwaihon@gmail.com>
2021-12-11 15:07:46 +0800

    Prevent guest users from visiting the create page for Book using an ESA mixin

--- app/routes/book/create.js
+++ app/routes/book/create.js
@@ -1,6 +1,10 @@
 import Route from '@ember/routing/route';
+// eslint-disable-next-line ember/no-mixins
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default class BookCreateRoute extends Route {
+export default class BookCreateRoute extends Route.extend(
+  AuthenticatedRouteMixin
+) {
   model() {
     return {
       title: '',
