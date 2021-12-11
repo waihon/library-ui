16ba7c15093f9ec776e105e21cff8f7f7839d289
Waihon Yew <yewwaihon@gmail.com>
2021-12-11 13:34:04 +0800

    Prevent guest users from visiting the create page for Author using an ESA mixin

--- app/routes/author/create.js
+++ app/routes/author/create.js
@@ -1,15 +1,10 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
+// eslint-disable-next-line ember/no-mixins
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default class AuthorCreateRoute extends Route {
-  @service session;
-
-  beforeModel() {
-    if (!this.session.isAuthenticated) {
-      this.transitionTo('login');
-    }
-  }
-
+export default class AuthorCreateRoute extends Route.extend(
+  AuthenticatedRouteMixin
+) {
   // The model function is run everytime we enter the route
   model() {
     return {
