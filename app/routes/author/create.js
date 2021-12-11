81a9de2172d80581558c8a923dccd022336e07e8
Waihon Yew <yewwaihon@gmail.com>
2021-12-11 13:10:47 +0800

    Prevent guest users from visiting the create page for Author

--- app/routes/author/create.js
+++ app/routes/author/create.js
@@ -1,6 +1,15 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class AuthorCreateRoute extends Route {
+  @service session;
+
+  beforeModel() {
+    if (!this.session.isAuthenticated) {
+      this.transitionTo('login');
+    }
+  }
+
   // The model function is run everytime we enter the route
   model() {
     return {
