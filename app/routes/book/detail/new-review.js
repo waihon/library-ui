93622ed96abb8cce38085fc9820e448425ae6df7
Waihon Yew <yewwaihon@gmail.com>
2021-12-11 15:09:36 +0800

    Prevent guest users from visiting the create page for Review using an ESA mixin

--- app/routes/book/detail/new-review.js
+++ app/routes/book/detail/new-review.js
@@ -1,6 +1,10 @@
 import Route from '@ember/routing/route';
+// eslint-disable-next-line ember/no-mixins
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default class BookDetailNewReviewRoute extends Route {
+export default class BookDetailNewReviewRoute extends Route.extend(
+  AuthenticatedRouteMixin
+) {
   model() {
     let book = this.modelFor('book.detail');
 
