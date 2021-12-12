91c5c53a53295f38a3bfeb6b16c357a65273d1e6
Waihon Yew <yewwaihon@gmail.com>
2021-12-12 14:24:20 +0800

    Use the author edit authorization rule defined in an Ember-can ability class

--- app/routes/author/edit.js
+++ app/routes/author/edit.js
@@ -6,7 +6,7 @@ import { inject as service } from '@ember/service';
 export default class AuthorEditRoute extends Route.extend(
   AuthenticatedRouteMixin
 ) {
-  @service('current-user') currentUser;
+  @service abilities;
 
   model({ id }) {
     return this.store.findRecord('author', id);
@@ -15,8 +15,8 @@ export default class AuthorEditRoute extends Route.extend(
   // afterModel function allows us to run logic after loading the model
   // but before rendering the template and finishing the transition.
   afterModel(model) {
-    if (model.username !== this.currentUser.user.username) {
-      this.transitionTo('author.detail', model.id);
+    if (!this.abilities.can('edit author', model)) {
+      return this.transitionTo('author.detail', model.id);
     }
   }
 }
