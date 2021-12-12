b7680a4c301f82588c472d036360bdaf24f311f6
Waihon Yew <yewwaihon@gmail.com>
2021-12-12 10:46:22 +0800

    Redirect guest users to login route when they try to edit an author

--- app/routes/author/edit.js
+++ app/routes/author/edit.js
@@ -1,6 +1,10 @@
 import Route from '@ember/routing/route';
+// eslint-disable-next-line ember/no-mixins
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default class AuthorEditRoute extends Route {
+export default class AuthorEditRoute extends Route.extend(
+  AuthenticatedRouteMixin
+) {
   model({ id }) {
     return this.store.findRecord('author', id);
   }
