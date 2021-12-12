70b8c0fab995b6d0edde257fad9ed433e1562cbe
Waihon Yew <yewwaihon@gmail.com>
2021-12-12 14:59:02 +0800

    Define book edit authorization rule in an Ember-can ability class

---
+++ app/abilities/book.js
@@ -0,0 +1,16 @@
+import { Ability } from 'ember-can';
+import { inject as service } from '@ember/service';
+// eslint-disable-next-line ember/no-computed-properties-in-native-classes
+import { computed } from '@ember/object';
+
+export default class BookAbility extends Ability {
+  @service('current-user') currentUser;
+
+  // Dependencies are required to be declared statically in computed properties.
+  // Properties accessed within the computed property function that are not
+  // listed out are assumed to be missing dependencies.
+  @computed('currentUser.user.username', 'model.username')
+  get canEdit() {
+    return this.currentUser.user.username === this.model.username;
+  }
+}
