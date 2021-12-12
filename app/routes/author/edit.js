9fa000b059a7c5bd35d58d39b15983656f6342c6
Waihon Yew <yewwaihon@gmail.com>
2021-12-12 11:46:39 +0800

    Redirect non-creator to author detail page when trying to edit

--- app/routes/author/edit.js
+++ app/routes/author/edit.js
@@ -1,11 +1,22 @@
 import Route from '@ember/routing/route';
 // eslint-disable-next-line ember/no-mixins
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import { inject as service } from '@ember/service';
 
 export default class AuthorEditRoute extends Route.extend(
   AuthenticatedRouteMixin
 ) {
+  @service('current-user') currentUser;
+
   model({ id }) {
     return this.store.findRecord('author', id);
+  }
+
+  // afterModel function allows us to run logic after loading the model
+  // but before rendering the template and finishing the transition.
+  afterModel(model) {
+    if (model.username !== this.currentUser.user.username) {
+      this.transitionTo('author.detail', model.id);
+    }
   }
 }
