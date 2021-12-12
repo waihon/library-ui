55427e53dbfcc7411e131bdcf6b28f80f5bea799
Waihon Yew <yewwaihon@gmail.com>
2021-12-12 15:00:03 +0800

    Use the book edit authorization rule defined in an Ember-can ability class

--- app/routes/book/edit.js
+++ app/routes/book/edit.js
@@ -1,7 +1,16 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class BookEditRoute extends Route {
+  @service abilities;
+
   model({ id }) {
     return this.store.findRecord('book', id);
+  }
+
+  afterModel(model) {
+    if (this.abilities.cannot('edit book', model)) {
+      return this.transitionTo('book.detail', model.id);
+    }
   }
 }
