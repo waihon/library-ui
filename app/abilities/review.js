56d5a60d5eca99c0cc4ba24ad60511cdbc1c51dd
Waihon Yew <yewwaihon@gmail.com>
2021-12-15 00:32:14 +0800

    Add a rule to review ability such that only authenticated user could create review

--- app/abilities/review.js
+++ app/abilities/review.js
@@ -1,3 +1,13 @@
 import { Ability } from 'ember-can';
+import { inject as service } from '@ember/service';
+// eslint-disable-next-line ember/no-computed-properties-in-native-classes
+import { computed } from '@ember/object';
 
-export default class extends Ability {}
+export default class ReviewAbility extends Ability {
+  @service session;
+
+  @computed('session.isAuthenticated')
+  get canCreate() {
+    return this.session.isAuthenticated;
+  }
+}
